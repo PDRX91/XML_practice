fc8e447bba1c478a09055c589c74cc25e52957a6
PDRX91 <parker.rebensdorf@gmail.com>
2019-01-28 18:17:59 -0800

    all objectives complete but html parsing is very manual

--- dummy.php
+++ dummy.php
@@ -10,16 +10,24 @@ $subnode1 = $node1->addChild('book');
 $subnode1->addAttribute('id', 'hp');
 $subnode1->addAttribute('name', 'Harry Potter');
 
-$inner_node2 = $subnode1->addChild('author');
-$inner_node2->addAttribute('id', 'author_hp');
-$inner_node2->addAttribute('name', 'JK Rowling');
+$inner_node1_1 = $subnode1->addChild('author');
+$inner_node1_1->addAttribute('id', 'author_hp');
+$inner_node1_1[0] = "JK Rowling";
+
+$inner_node1_2 = $subnode1->addChild('title');
+$inner_node1_2->addAttribute('id', 'title_hp');
+$inner_node1_2[0] = "Harry Potter and the Deathly Hallows";
 
 $subnode2 = $node1->addchild('book');
 $subnode2->addAttribute('id', 'lotr');
 $subnode2->addAttribute('name', 'Lord of The Rings');
 
-$inner_node1 = $subnode2->addChild('author');
-$inner_node1->addAttribute('id', 'author_lotr');
-$inner_node1->addAttribute('name', 'JRR Tolkien');
+$inner_node2_1 = $subnode2->addChild('author');
+$inner_node2_1->addAttribute('id', 'author_lotr');
+$inner_node2_1[0] = 'JRR Tolkien';
+
+$inner_node2_2 = $subnode2->addChild('title');
+$inner_node2_2->addAttribute('id', 'title_lotr');
+$inner_node2_2[0] = 'Return of the King';
 
 echo $xml->asXML('dummy_data2.xml');
